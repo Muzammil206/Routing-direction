e99fb2b5762523e4e9fc9dbe3bb42f6a68f3aedf
Muzammil206 <ismailmuzammil206@gmail.com>
2023-10-08 04:06:50 +0100

    new features added

--- map.js
+++ map.js
@@ -1,7 +1,18 @@
 
 
+const button = document.querySelector('.settings');
+const closeButton = document.querySelector('.close');
+const quickSettings = document.querySelector('.quick-settings');
 
+button.addEventListener('click', () => {
+  console.log('hi')
+    quickSettings.classList.remove('none');
+    quickSettings.classList.remove('hide');
+});
 
+closeButton.addEventListener('click', () => {
+    quickSettings.classList.add('hide');
+});
     
 
 
@@ -96,7 +107,7 @@ require([
  
 
 
-//  SearchBar(map)
+ SearchBar(map)
 
 
  function DisplayDirection() {
